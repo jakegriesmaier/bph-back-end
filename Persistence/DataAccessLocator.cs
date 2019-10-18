dffeb7b9bcd23f7c9518e20bdb7ebccffefdb1e3
jake Griesmaier <jakegriesmaier@gmail.com>
2019-10-18 12:56:45 -0500

    refactored solution dependencies for testing purposes

---
+++ Persistence/DataAccessLocator.cs
@@ -0,0 +1,11 @@
+﻿using Model.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class DataAccessLocator : DataAccessLocatorBase
+    {
+    }
+}
---
+++ Presentation/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.DataAccess;
+using Persistence;
+
+namespace Presentation.Controllers
+{
+    internal abstract class BaseController : ControllerBase
+    {
+
+        // used for accessing database
+        private DataAccessLocatorBase _dataAccessLocator;
+        protected internal DataAccessLocatorBase DataAccessLocator => _dataAccessLocator ?? (new DataAccessLocator());
+
+    }
+}
