e1cbb86db61791d3a05976534a55f4bb78aa0694
Jake Schaum <schaum@wisc.edu>
2019-10-30 22:37:09 -0500

    no message

--- Model/Entities/User.cs
+++ Model/Entities/User.cs
@@ -7,9 +7,9 @@ namespace Model.Entities
 {
     public class User
     {
-        public string Username { get; set; }
+        public string UserId { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
-        public string Email { get; set; }// make this a formatted string or add validator for email
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public double? Height { get; set; }
