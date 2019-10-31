b01f83721abd085a45cfa5c75f31fb3bc44109f9
jake Griesmaier <jakegriesmaier@gmail.com>
2019-10-30 21:41:40 -0500

    Added Entities for plans, removed README in the folder

---
+++ Model/Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Entities
+{
+    public class Comment
+    {
+        public string CommentId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public User CreatedBy { get; set; }
+        public string Description { get; set; }
+    }
+}
