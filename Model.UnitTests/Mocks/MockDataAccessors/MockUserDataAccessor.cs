fb18832011def50b1518066af77c8ffdc1ba9a63
Jake Schaum <schaum@wisc.edu>
2019-10-31 15:09:34 -0500

    test/mocks

--- Model.UnitTests/Mocks/MockDataAccessors/MockUserDataAccessor.cs
+++ Model.UnitTests/Mocks/MockDataAccessors/MockUserDataAccessor.cs
@@ -1,4 +1,5 @@
 ﻿using Model.DataAccess.BaseAccessors;
+using Model.Models.Validators;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,19 +10,19 @@ namespace Model.UnitTests.Mocks.MockDataAccessors
     public class MockUserDataAccessor : UserDataAccessorBase
     {
         // return fake calls to the database here for testing
-        protected override Task CreateUserCore(string email, string password)
+        protected override async Task CreateUserCore(string email, string password)
         {
-            throw new NotImplementedException();
+            return;
         }
 
-        protected override Task LoginUserCore(string email, string password)
-        {
-            throw new NotImplementedException();
+        protected override async Task LoginUserCore(string email, string password)
+        { 
+            return;
         }
 
-        protected override Task LogoutUserCore()
+        protected override async Task LogoutUserCore()
         {
-            throw new NotImplementedException();
+            return;
         }
     }
 }
---
+++ Model.UnitTests/Tests/UserControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Model.UnitTests
+{
+    public class UserControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void LoginUserTest()
+        {
+            Assert.AreEqual(true, CredentialsValidator.ValidateEmail(goodEmail), "Marked a good email as bad");
+        }
+    }
+}
