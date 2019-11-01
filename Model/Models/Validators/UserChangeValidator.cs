f57f70f432066e23129396852002b9ac0e6e71ec
Jake Schaum <schaum@wisc.edu>
2019-11-01 14:09:10 -0500

    renaming of UserValidation

---
+++ Model/Models/Validators/UserChangeValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Model.Entities;
+
+namespace Model.Models.Validators
+{
+    public class UserChangeValidator
+    {
+       
+        public static bool ValidateUpdateUser(User user)
+        {
+            if (user.Email == null || user.UserId == null)
+            {
+                return false;
+            }
+           
+            return true;
+        }
+    }
+}
