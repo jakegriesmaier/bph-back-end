654b7e88fc1732cab725c9354a917cdbe17bb8b1
jake Griesmaier <jakegriesmaier@gmail.com>
2019-11-01 12:47:31 -0500

    created class setups and folder for Coach Tests

---
+++ Model.UnitTests/Tests/Coach/CreateWorkoutTest.cs
@@ -0,0 +1,21 @@
+﻿using Model.Models;
+using Model.UnitTests.Mocks;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.UnitTests.Tests.Coach
+{
+    public class CreateWorkoutTest
+    {
+        private CoachModel _coachModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            var dataAccessLocator = new MockDataAccessLocator();
+            _coachModel = new CoachModel(dataAccessLocator);
+        }
+    }
+}
