64eca0c32dfecd2ca9e7a8075fe5d3aaba3cf7d5
jake Griesmaier <jakegriesmaier@gmail.com>
2019-11-10 13:21:56 -0600

    added [FromBody] for consistency and moved all API input data to the body of the request

---
+++ Presentation/Controllers/CoachPresenter/InputData/GetWorkoutInputData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers.CoachPresenter.InputData
+{
+    public class GetWorkoutInputData
+    {
+        public string WorkoutId { get; set; }
+    }
+}
