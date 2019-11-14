cea23d3e2740405ea7d367e85171f156d113aa56
Jake Schaum <schaum@wisc.edu>
2019-11-14 00:12:20 -0600

    easiest traineemodel of my life

---
+++ Model/Models/TraineeModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Model.DataAccess;
+using Model.Entities;
+using Model.Exceptions;
+using Model.Models.Validators;
+
+namespace Model.Models
+{
+    public class TraineeModel : ModelBase
+    {
+        public TraineeModel(DataAccessLocatorBase dataAccessLocator) : base(dataAccessLocator)
+        {
+        }
+
+        public async Task<Plan> GetPlan(string planId)
+        {
+            if (!PlanValidator.ValidateGetPlan(planId))
+            {
+                throw new InsufficientInformationException(ExceptionMessages.INVALID_GET_PLAN_PARAMS);
+            }
+            return await PlanDataAccessor.GetPlan(planId);
+        }
+
+        public async Task<Workout> GetWorkout(string workoutId)
+        {
+            if (!WorkoutValidator.ValidateGetWorkout(workoutId))
+            {
+                throw new InsufficientInformationException(ExceptionMessages.INVALID_GET_WORKOUT_PARAMS);
+            }
+            return await WorkoutDataAccessor.GetWorkout(workoutId);
+        }
+    }
+}
