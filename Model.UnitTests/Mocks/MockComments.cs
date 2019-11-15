2f5fc0db9159c0e7750606d168189f1996dee2ff
gdarin <gdarin@wisc.edu>
2019-11-15 16:14:48 -0600

    work on Comments

---
+++ Model.UnitTests/Mocks/MockComments.cs
@@ -0,0 +1,51 @@
+﻿using Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Model.UnitTests.Mocks
+{
+    public static class MockComments
+    {
+        public static Comment GetComment(string id)
+        {
+            if (id == Comment1().CommentId)
+            {
+                return Comment1();
+            }
+            return new Comment();
+        }
+
+        public static Comment Comment1()
+        {
+            return new Comment
+            {
+                CommentId = "comment-1",
+                CreatedDate = DateTime.Now,
+                CreatedById = MockUsers.Coach().UserId,
+                Description = "comment-1 descript"
+            };
+        }
+        public static Comment creationNullCommentId()
+        {
+            return new Comment
+            {
+                CommentId = null,
+                CreatedDate = DateTime.Now,
+                CreatedById = MockUsers.Coach().UserId,
+                Description = "pizza"
+            };
+        }
+        public static Comment creationWithoutDescription()
+        {
+            return new Comment
+            {
+                CommentId = "Spaghetti",
+                CreatedDate = DateTime.Now,
+                CreatedById = MockUsers.Coach().UserId,
+                Description = null
+            };
+        }
+    }
+}
---
+++ Model/Models/Validators/CommentValidator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Model.Entities;
+
+namespace Model.Models.Validators
+{
+    public class CommentValidator
+    {
+        public static bool ValidateCreateComment(Comment comment, string ownerId)
+        {
+            if (comment.CommentId != null)
+            {
+                return false;
+            }
+            if (ownerId != null)
+            {
+                return false;
+            }
+            if (comment.Description == null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool ValidateGetComment(string commentId)
+        {
+            if (commentId == null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool ValidateUpdateComment(Comment comment)
+        {
+            if (comment.CommentId == null)
+            {
+                return false;
+            }
+            if (comment.Description == null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool ValidateDeleteComment(string commentId)
+        {
+            if (commentId == null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool ValidateGetComments(string ownerId)
+        {
+            if (ownerId == null)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
