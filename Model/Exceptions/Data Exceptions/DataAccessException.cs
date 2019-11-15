f9aa3f6204fdf7044adaf4fa8382197455207528
Jake Schaum <schaum@wisc.edu>
2019-11-15 12:34:50 -0600

    exceptions and stuff.

---
+++ Model/Exceptions/Data Exceptions/DataAccessException.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+
+namespace Model.Exceptions.DataExceptions
+{
+    /// <summary>
+    /// Exception thrown when the call should not have been called from the 
+    /// current user.
+    /// </summary>
+    public class DataAccessException : CustomException
+    {
+        private const HttpStatusCode code = HttpStatusCode.Unauthorized;
+
+        public DataAccessException() : base()
+        {
+            this.StatusCode = code;
+            this.DeveloperMessage = "";
+        }
+
+        public DataAccessException(string devMessage, string message) : base(devMessage, message)
+        {
+            this.StatusCode = code;
+            this.DeveloperMessage = devMessage;
+        }
+
+        public DataAccessException(string devMessage, string message, Exception innerException) : base(devMessage, message, innerException)
+        {
+            this.StatusCode = code;
+            this.DeveloperMessage = devMessage;
+        }
+    }
+}
+
+
+using System;
+using System.Net;
+
+namespace Model.Exceptions.DataExceptions
+{
+    /// <summary>
+    /// Exception thrown when a GET call has no associated data returned.
+    /// </summary>
+    public class DataDoesNotExistException : CustomException
+    {
+        private const HttpStatusCode code = HttpStatusCode.NotFound;
+
+        public DataDoesNotExistException() : base()
+        {
+            this.StatusCode = code;
+            this.DeveloperMessage = "";
+        }
+
+        public DataDoesNotExistException(string devMessage, string message) : base(devMessage, message)
+        {
+            this.StatusCode = code;
+            this.DeveloperMessage = devMessage;
+        }
+
+        public DataDoesNotExistException(string devMessage, string message, Exception innerException) : base(devMessage, message, innerException)
+        {
+            this.StatusCode = code;
+            this.DeveloperMessage = devMessage;
+        }
+    }
+}
---
+++ Model/Exceptions/Data Exceptions/DuplicateDataException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace Model.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when the call tries to add data that already exists
+    /// </summary>
+    public class DuplicateDataException : CustomException
+    {
+        private const HttpStatusCode code = HttpStatusCode.409;
+
+        public DuplicateDataException() : base()
+        {
+            this.StatusCode = code;
+            this.DeveloperMessage = "";
+        }
+
+        public DuplicateDataException(string devMessage, string message) : base(devMessage, message)
+        {
+            this.StatusCode = code;
+            this.DeveloperMessage = devMessage;
+        }
+
+        public DuplicateDataException(string devMessage, string message, Exception innerException) : base(devMessage, message, innerException)
+        {
+            this.StatusCode = code;
+            this.DeveloperMessage = devMessage;
+        }
+    }
+}
