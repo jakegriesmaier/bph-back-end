c68aaf99885b8e6ddfe2ec36d892861485321a8f
gdarin <gdarin@wisc.edu>
2019-12-05 17:07:09 -0600

    refactored User and Workout Validators.  All Validators refactored now

--- Model/Models/Validators/UserValidator.cs
+++ Model/Models/Validators/UserValidator.cs
@@ -5,10 +5,10 @@ namespace Model.Models.Validators
 {
     public class UserValidator
     {
-
+        #region Validators
         public static bool ValidateGetUser(string userId)
         {
-            if (userId == null)
+            if (!UserExists(userId))
             {
                 return false;
             }
@@ -17,12 +17,31 @@ namespace Model.Models.Validators
 
         public static bool ValidateUpdateUser(User user)
         {
-   
-            if (user.Email == null || user.UserId == null)
+            if (!UserExists(user))
+            {
+                return false;
+            }
+            if(!EmailExists(user))
             {
                 return false;
             }
             return true;
         }
+        #endregion
+
+        #region Helper Functions
+        private static bool UserExists(string userId)
+        {
+            return (userId != null) && (userId != "");
+        }
+        private static bool UserExists(User user)
+        {
+            return (user.UserId != null) && (user.UserId != "");
+        }
+        private static bool EmailExists(User user)
+        {
+            return (user.Email != null) && (user.Email != "");
+        }
+        #endregion
     }
 }
